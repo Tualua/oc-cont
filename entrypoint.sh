d3354716b29af13ebc85597ed6d40891683337c2
Dmitry Popovich <32481693+Tualua@users.noreply.github.com>
2024-11-11 16:51:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
 
-echo $PASSWORD | /usr/bin/openconnect $SERVER --user=$USER --passwd-on-stdin --queue-len=15 --background
+echo $PASSWORD | /usr/bin/openconnect $SERVER --user=$USER --passwd-on-stdin --queue-len=15
